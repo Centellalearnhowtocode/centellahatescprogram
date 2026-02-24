21a05e46d63415a5c6a327718175b899848fae6d
centellalearnshowtocode <chornay126@gmail.com>
2026-02-24 21:01:03 +0700

    Add files via upload

---
+++ main.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+int main (){
+  //Arithmetic Operator
+   int x= 10;
+ int y=2;
+ int z = 0; 
+  z= x % y; //remain value after devision
+  x++; //++x same thing
+  --x; //x-- same thing
+  x-=3; // the value of x minus 3
+  x*=4; // the value of x from top time 4
+  printf("%d\n",z);
+  printf("%d",x); //=11+1-1-3
+
+
+
+  return 0;
+
+}
