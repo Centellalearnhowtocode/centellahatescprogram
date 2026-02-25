f50858cce8b213e1f6d114d9067cbda01cbe5a44
centellalearnshowtocode <chornay126@gmail.com>
2026-02-25 11:05:50 +0700

    Update Excercise1.c
    FUCK IT WHAT THE FUCK IS THIS WHY DO I HAVE TO DO THIS TO MYSELF

--- Excercise1.c
+++ Excercise1.c
@@ -1,5 +1,42 @@
 #include <stdio.h>
 int main (){
-  return 0;
+  //Excercise 1
+  float baseFareDollars;
+  float distanceCostDollars;
+  float distanceKm;
+  float ratePeKmDollars;
+  float ratePerMinuteDollars;
+  int waitingMinute;
 
+  float waitingCostDollars;
+  float totalFareeDollars;
+
+  distanceCostDollars = distanceKm*ratePeKmDollars;
+  waitingCostDollars=waitingMinute*ratePerMinuteDollars;
+  totalFareeDollars=baseFareDollars+distanceCostDollars+waitingCostDollars;
+
+  printf("\nBase Fare ($)\t:\t");
+  scanf("%f",&baseFareDollars);
+  printf("\nDistance (km)\t:\t");
+  scanf("%d",&distanceKm);
+  printf("\nRate per km ($) :\t");
+  scanf("%f",&ratePeKmDollars);
+  printf("\nWaitng (min)\t:\t");
+  scanf("%f",&waitingMinute);
+  printf("Rate per min ($):\t");
+  scanf("%f",&ratePerMinuteDollars);
+  
+  
+printf("==============================================");
+printf("TAXI FARE REPORT");
+printf("==============================================");
+ printf("\nBase Fare ($)\t:\t%f",ratePerMinuteDollars);
+ printf("\nDistance (km)\t:\t%f",distanceKm);
+ printf("\nRate per km ($) :\t%f",ratePeKmDollars);
+ printf("\nWaitng (min)\t:\t%f",waitingMinute);
+ printf("\nRate per min ($):\t%f",&ratePerMinuteDollars);
+ printf("\nTOTAL FARE ($):%f",totalFareeDollars);
+ 
+ //Excercise 2
+  return 0;
 }
