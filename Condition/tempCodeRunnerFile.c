f19e417d54d824e04e80093927f91dc8363be29d
centellalearnhowtocode <chornay126@gmail.com>
2026-04-15 21:17:28 +0700

    commit to main

---
+++ Condition/tempCodeRunnerFile.c
@@ -0,0 +1,16 @@
+    printf("Enter your option: ");
+    scanf("%d", &option);
+
+  if (password==4321){
+    printf("Your Password is correct.");
+  }
+  else {
+    printf("Password is incorrect.");
+  }
+  if (option ==1){
+    printf("Your balance is $%d.", balance);
+  }
+  else{
+    printf("\nYour your access denied.");
+  }
+    return 0;
